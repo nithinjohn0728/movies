348df528c960fc42fdad54e2acea0991a3beb5cb
Nithin John Cherian <nithinjohn0728@gmail.com>
2023-05-11 15:36:09 +0100

    initializing react router and header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="logo">Movie App</div>
+      <div className="user-image">
+        <img src = {} alt = "user" />
+      </div>
+
+    </div>
+  );
 };
 
 export default Header;
