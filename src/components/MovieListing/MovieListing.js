46ce68489b925ca6750d4d73da3f8c4af523aa4d
Nithin John Cherian <nithinjohn0728@gmail.com>
2023-05-11 18:52:46 +0100

    useSelector,useDispatch and updated movieListing

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { getAllMovies } from "../../features/movies/movieSlice";
+import { useSelector } from "react-redux";
+import MovieCard from "../MovieCard/MovieCard";
 
 const MovieListing = () => {
-  return <div>MovieListing</div>;
+  const movies = useSelector(getAllMovies);
+  let renderMovies = "";
+  renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => {
+        <MovieCard key={index} data={movie} />;
+      })
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+      </div>
+    </div>
+  );
 };
 
 export default MovieListing;
