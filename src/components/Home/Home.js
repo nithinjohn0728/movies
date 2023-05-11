37ffac98a5c21dd0f4706bf62273aabc838a942d
Nithin John Cherian <nithinjohn0728@gmail.com>
2023-05-11 17:04:05 +0100

    apicall usin useeffect

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { key } from "../../common/apis/movieApiKey";
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey = ${key}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log(err);
+        });
+      console.log("response from api", response);
+    };
+    fetchMovies();
+  }, []);
+
+  useEffect(() => {
+    const fetchMovies = async();
+  });
+  return (
+    <div>
+      <div className="banner"></div>;
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
